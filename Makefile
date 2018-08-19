fcb38e29004300eb8d03db5e3c5d3a71aecdad21
Matt Bieber <matt@spaceboy.co>
2018-08-19 12:41:21 -0700

    gh-pages

--- Makefile
+++
@@ -1,8 +0,0 @@
-REPORTER = dot
-TESTS = $(shell find test -name "*.spec.js")
-
-test:
-	mocha $(TESTS)
-
-.PHONY: test
-
--- template/build/js/themes.js
+++
@@ -1,9 +0,0 @@
-
-define({
-	'Default' : 'css/theme-default.css',
-	'Lettuce' : 'css/theme-lettuce.css',
-	'Flag'    : 'css/theme-flag.css',
-	'Orchard' : 'css/theme-orchard.css',
-	'Robotron': 'css/theme-robotron.css'
-
-});
